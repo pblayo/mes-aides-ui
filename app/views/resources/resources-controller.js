cf1802b0fc517b1c8be4506a3694700f0677adf7
Matti Schneider <matti.schneider@data.gouv.fr>
2015-07-28 15:10:16 +0200

    Update graph based on inputs

--- app/views/resources/resources-controller.js
+++ app/views/resources/resources-controller.js
@@ -33,31 +33,39 @@ angular.module('MAResources')
     $scope.options.series = [];
     $scope.options.stacks = [ { axis: 'y', series: [] } ];
 
-    var data = {};
+    $scope.$watch(function() {
+        return MASituation.individus[0].resources;
+    }, function() {
+        var data = {};
+        $scope.options.stacks[0].series = [];
+        $scope.options.series = [];
 
-    angular.forEach(MASituation.individus[0].resources, function(entries, id) {
-        entries.forEach(function(entry, index) {
-            var entryId = id + ':' + index;
+        angular.forEach(MASituation.individus[0].resources, function(entries, id) {
+            entries.forEach(function(entry, index) {
+                var entryId = id + ':' + index;
 
-            $scope.options.stacks[0].series.push(entryId);
-            $scope.options.series.push({
-                id: entryId,
-                y: entryId,
-                label: MAResourcesList[id].label,
-                color: 'hsl(' + (45 * id.length) + ',100%,' + (60 - 15 * index ) + '%)',
-                thickness: '0px',
-                type: 'area'
-            });
-            angular.forEach(entry.earnedOn, function(earned, date) {
-                data[date] = data[date] || { date: new Date(date) };
-                data[date][entryId] =  data[date][entryId] || 0;
-                data[date][entryId] += earned && entry.value;
-            });
-        })
-    });
+                $scope.options.series.push({
+                    id: entryId,
+                    y: entryId,
+                    label: MAResourcesList[id].label,
+                    color: 'hsl(' + (45 * id.length) + ',100%,' + (60 - 15 * index ) + '%)',
+                    thickness: '0px',
+                    type: 'area'
+                });
 
-    $scope.resources = [];
-    angular.forEach(data, function(point) {
-        $scope.resources.push(point);  // assumes that objects keeps insertion order
-    });
+                $scope.options.stacks[0].series.push(entryId);
+
+                angular.forEach(entry.earnedOn, function(earned, date) {
+                    data[date] = data[date] || { date: new Date(date) };
+                    data[date][entryId] =  data[date][entryId] || 0;
+                    data[date][entryId] += earned && entry.value;
+                });
+            })
+        });
+
+        $scope.resources = [];
+        angular.forEach(data, function(point) {
+            $scope.resources.push(point);  // assumes that objects keeps insertion order
+        });
+    }, true);
 });
