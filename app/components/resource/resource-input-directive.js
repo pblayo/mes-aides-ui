77beb96c159e9f2e85ee7d79b7e93d37db955337
Matti Schneider <matti.schneider@data.gouv.fr>
2015-07-31 10:42:54 +0200

    Focus amount input upon resource input appearance

--- app/components/resource/resource-input-directive.js
+++ app/components/resource/resource-input-directive.js
@@ -1,5 +1,5 @@
 angular.module('MAResources')
-.directive('maResourceInput', function(MAResourcesList) {
+.directive('maResourceInput', function($timeout, MAResourcesList) {
     return {
         restrict: 'E',
         templateUrl: '/components/resource/resource-input.html',
@@ -8,6 +8,10 @@ angular.module('MAResources')
         },
         link: function($scope, element, attrs) {
             $scope.label = MAResourcesList[$scope.resource.type].label;
+
+            var amountInput = element[0].querySelector('input[type="number"]');
+            amountInput.focus();
+            $timeout(amountInput.setSelectionRange.bind(amountInput, 0, 99));  // wrapping in timeout due to https://code.google.com/p/chromium/issues/detail?id=32865
         }
     }
 });
