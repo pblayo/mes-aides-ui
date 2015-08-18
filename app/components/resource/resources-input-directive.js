925371aa78625701cdb835aeb2b0e5ec2d4287b7
Matti Schneider <matti.schneider@data.gouv.fr>
2015-08-18 19:11:49 +0200

    Add resources by type

--- app/components/resource/resources-input-directive.js
+++ app/components/resource/resources-input-directive.js
@@ -1,10 +1,14 @@
 angular.module('MAResources')
-.directive('maResourcesInput', function() {
+.directive('maResourcesInput', function(MAResourcesCategoriesList, MAResourcesList) {
     return {
         restrict: 'E',
         templateUrl: '/components/resource/resources-input.html',
         scope: {
             individu: '='
-        }
+        },
+        link: function($scope) {
+            $scope.categories = MAResourcesCategoriesList;
+            $scope.resourceTypes = MAResourcesList.byCategory;
+        },
     }
 });
