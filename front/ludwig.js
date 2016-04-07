22c5c11ace721b49ac32f396eaa0b7da0d3fad1f
Matti Schneider <matti.schneider@data.gouv.fr>
2016-04-07 12:55:38 +0200

    Update Ludwig to v0.3.0

--- front/ludwig.js
+++ front/ludwig.js
@@ -1,34 +1,30 @@
 import YAML from 'yamljs';
 
-import '../node_modules/sgmap.ludwig/dist/bundle.js';
+import '../node_modules/sgmap.ludwig/dist/ludwig.js';
 import store from './store';
 
 
 const ludwig = new window.Ludwig({
-    repoUrl: 'https://github.com/sgmap/openfisca-france/',
-    template: `Précisez si ce test provient :
-- [ ] d'un dossier réel
-- [ ] du résultat d'une simulation sur un outil métier
-
-NE MODIFIEZ PAS LE CONTENU CI-DESSOUS
----
-
-`,
+    repo: 'sgmap/openfisca-france',
+    branch: 'prod',
     prefix: 'test_',
-    web:{
-        acceptedTestsPath:'/tree/prod/openfisca_france/tests/mes-aides.gouv.fr',
-        addPath:'/new/prod/openfisca_france/tests/mes-aides.gouv.fr',
-        suggestedTestsPath:'/pulls?utf8=✓&q=is%3Apr+is%3Aopen',
-    },
 });
 
 document.getElementById('createTest').addEventListener('click', event => {
     const url = ludwig.generateSuggestionURL(
         store.getState().openfiscaSituation,
         store.getState().openfiscaSituation,
-        (template, actual, expected) => {
+        (actual, expected) => {
             const demoString = YAML.stringify(expected, 20, 2).slice(0, 300);  // work around URI size limit for demo purposes
-            return `${template}${demoString}[more content missing…]`;
+            return `Précisez si ce test provient :
+- [ ] d'un dossier réel
+- [ ] du résultat d'une simulation sur un outil métier
+
+NE MODIFIEZ PAS LE CONTENU CI-DESSOUS
+---
+${demoString}
+[more content missing…]
+`;
         }
     );
 
