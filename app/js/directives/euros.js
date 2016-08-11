4d40c437e45ced6e97085628b427e96a0068bdaf
Matti Schneider <matti.schneider@beta.gouv.fr>
2016-08-11 18:37:41 +0200

    Move selectOnFocus/zeroToEmpty directives to euros
    These directives were always used together, and were used on currency input fields only. #296 proved it was a defect not to include them. They should thus be part of the input for euros.

--- app/js/directives/euros.js
+++ app/js/directives/euros.js
@@ -17,8 +17,28 @@ angular.module('ddsCommon').directive('euros', function() {
             element.on('$destroy', formatter.destroy.bind(formatter));  // don't leak event listeners
 
             if (element[0].tagName != 'INPUT') {
-                element.text(formatter.properties.numeralFormatter.format(String(controller.$modelValue)));  // private API, always check it's still proper when updating Cleave)
+                return element.text(formatter.properties.numeralFormatter.format(String(controller.$modelValue)));  // private API, always check it's still proper when updating Cleave)
             }
+
+            var input = element;
+
+            input.on('click', function() {
+                this.select();
+            });
+
+            input.on('focus', function() {
+                if ('0' === controller.$viewValue) {
+                    controller.$setViewValue(null);
+                    controller.$render();
+                }
+            });
+
+            input.on('blur', function() {
+                if (null === controller.$viewValue) {
+                    controller.$setViewValue('0');
+                    controller.$render();
+                }
+            });
 
             // formatter.setRawValue(controller.$modelValue || 0);
             // element.attr('inputmode', 'numeric');
